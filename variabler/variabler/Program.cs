50e7b54f3b29aa6878ca6e204e04c672aa532be0
StepparN <kalle.favoryt@elev.ga.ntig.se>
2020-09-11 14:59:34 +0200

    a

---
+++ variabler/variabler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace variabler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in namn:");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Namn:" + namn);
+
+            Console.WriteLine("Skriv in din ålder:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ålder:" + age);
+
+            Console.WriteLine("Är du vid liv, Ja / Nej ?");
+            Console.ReadLine();
+            bool Ja = true;
+            bool Nej = false;
+            Console.WriteLine("Är du vid liv?" + Ja);
+            Console.WriteLine("Är du vid liv?" + Nej);
+
+        }
+    }
+}
