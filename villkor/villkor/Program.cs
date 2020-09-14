37af825c562416587a4e5a754989f0293d85c353
StepparN <kalle.favoryt@elev.ga.ntig.se>
2020-09-14 08:32:07 +0200

    villkor

---
+++ villkor/villkor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace villkor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in din ålder");
+            var age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 20)
+            {
+                Console.WriteLine("Du får köpa alkohol");
+            }
+            else
+            {
+                Console.WriteLine("Du får inte köpa alkohol");
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+                
+        }
+    }
+}
