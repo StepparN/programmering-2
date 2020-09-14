61cf41e6fc829e127175e9b05cc031db5434bc32
StepparN <kalle.favoryt@elev.ga.ntig.se>
2020-09-14 10:31:39 +0200

    villkor

--- villkor/villkor/Program.cs
+++ villkor/villkor/Program.cs
@@ -20,6 +20,32 @@ namespace villkor
             {
                 Console.WriteLine(i);
             }
+            int sprit = 3;
+            switch (sprit)
+            {
+                case 1:
+                    Console.WriteLine("Gin");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Vodka");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Tequila");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Whisky");
+                    break;
+            }
+
+            int age1 = 18;
+
+            while (age1 < 20){
+                Console.WriteLine("Du får snart köpa alkohol");
+                age1++;
+            }
                 
         }
     }
