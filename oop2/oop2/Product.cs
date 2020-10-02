b81f725b837aa7066e459dc4f566824f6b20a983
StepparN <kalle.favoryt@elev.ga.ntig.se>
2020-10-02 15:24:57 +0200

    a

---
+++ oop2/oop2/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop2
+{
+    class Product
+    {
+    }
+}
---
+++ oop2/oop2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop2
+{
+    class Customer
+    {
+        List<Product> cart = new List<Product>();
+        
+            
+        }
+    }
+
