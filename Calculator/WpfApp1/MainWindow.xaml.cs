ff62e89f24fadee292fa1e03013c8baef5f14962
StepparN <kalle.favoryt@elev.ga.ntig.se>
2020-11-13 15:18:26 +0100

    kuh

--- Calculator/WpfApp1/MainWindow.xaml.cs
+++ Calculator/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button button)
+            {
+                switch (button.Content)
+                {
+                    case "0":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                    case "+":
+                    case "-":
+                    case "x":
+                    case "/":
+                        Numbers.Text += button.Content;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
     }
 }
