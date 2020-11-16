cf9dc690c1b3c9f159bd774aa94f6a3689f5ff6c
StepparN <kalle.favoryt@elev.ga.ntig.se>
2020-11-16 12:40:43 +0100

    oij

--- Calculator/WpfApp1/MainWindow.xaml.cs
+++ Calculator/WpfApp1/MainWindow.xaml.cs
@@ -41,16 +41,36 @@ namespace WpfApp1
                     case "7":
                     case "8":
                     case "9":
-                    case "+":
-                    case "-":
-                    case "x":
-                    case "/":
                         Numbers.Text += button.Content;
                         break;
                     default:
                         break;
                 }
             }
+        }
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            Numbers.Text = String.Empty;
+        }
+        private void Button_Calculate(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void Button_Addition(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Subtraction(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Division(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Multiplication(object sender, RoutedEventArgs e)
+        {
+
         }
 
     }
