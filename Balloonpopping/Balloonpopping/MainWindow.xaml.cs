38ea2896e80f7e33e25285e57f21013a748459bb
StepparN <kalle.favoryt@elev.ga.ntig.se>
2021-05-28 11:01:18 +0200

    as

---
+++ Balloonpopping/Balloonpopping/MainWindow.xaml.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+
+namespace Balloonpopping
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        int speed = 3;
+        int intervals = 90;
+        Random rand = new Random();
+
+        List<Rectangle> itemRemover = new List<Rectangle>();
+
+        ImageBrush backgroundImage = new ImageBrush();
+
+        int balloonSkins;
+        int i;
+
+        int missedBalloons;
+
+        bool gameIsActive;
+
+        int score;
+
+        MediaPlayer player = new MediaPlayer();
+
+        //Här så sätts backgrundbilden in
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gameTimer.Tick += GameEngine;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+
+            backgroundImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/background-Image.jpg"));
+            MyCanvas.Background = backgroundImage;
+
+            RestartGame();
+
+        }
+
+        private void GameEngine(object sender, EventArgs e)
+        {
+            scoreText.Content = "Score: " + score;
+
+            intervals -= 10;
+
+            if (intervals < 1)
+            {
+                ImageBrush balloonImage = new ImageBrush();
+
+                balloonSkins += 1;
+
+                if (balloonSkins > 5)
+                {
+                    balloonSkins = 1;
+                }
+                //Switch-casen lägger till de 5 ballongernas olika färger
+                switch (balloonSkins)
+                {
+                    case 1:
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon1.png"));
+                        break;
+                    case 2:
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon2.png"));
+                        break;
+                    case 3:
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon3.png"));
+                        break;
+                    case 4:
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon4.png"));
+                        break;
+                    case 5:
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon5.png"));
+                        break;
+                }
+                //Denna kod avgör hur ballongerna ska se ut, alltså höjd och bredd
+                Rectangle newBalloon = new Rectangle
+                {
+                    Tag = "balloon",
+                    Height = 70,
+                    Width = 50,
+                    Fill = balloonImage
+                };
+
+                Canvas.SetLeft(newBalloon, rand.Next(50, 400));
+                Canvas.SetTop(newBalloon, 600);
+
+                MyCanvas.Children.Add(newBalloon);
+
+                intervals = rand.Next(90, 150);
+            }
+            //Det här gör att om ballongen kommer till toppen så läggs en missad ballong till
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                if ((string)x.Tag == "balloon")
+                {
+                    i = rand.Next(-5, 5);
+
+                    Canvas.SetTop(x, Canvas.GetTop(x) - speed);
+                    Canvas.SetLeft(x, Canvas.GetLeft(x) - (i * -1));
+                }
+
+                if (Canvas.GetTop(x) < 20)
+                {
+                    itemRemover.Add(x);
+
+                    missedBalloons += 1;
+                }
+
+            }
+
+            foreach(Rectangle y in itemRemover)
+            {
+                MyCanvas.Children.Remove(y);
+            }
+            //Det här kodblocket gör att om man missar mer än 10 ballonger så förlorar man och kan starta igen
+            //Har man mer än 10 poäng går det även snabbare
+            if (missedBalloons > 10)
+            {
+                gameIsActive = false;
+                gameTimer.Stop();
+                MessageBox.Show("Game over" + Environment.NewLine + "Click ok to play again");
+
+                RestartGame();
+            }
+
+            if (score > 10)
+            {
+                speed = 7;
+            }
+
+
+        }
+        //Det här gör att när man trycker på musen och poppar en ballong så spelas ett ljud samt att man får ett poäng
+        private void PopBalloons(object sender, MouseButtonEventArgs e)
+        {
+            if (gameIsActive)
+            {
+                if (e.OriginalSource is Rectangle)
+                {
+                    Rectangle activeRec = (Rectangle)e.OriginalSource;
+
+                    player.Open(new Uri("../../files/pop-sound.mp3", UriKind.RelativeOrAbsolute));
+                    player.Play();
+
+                    MyCanvas.Children.Remove(activeRec);
+
+                    score += 1;
+                }
+            }
+
+
+        }
+        //Kodblocket nedan startar spelet och sätter alla poäng, intervallen mellan ballongerna, och hastigheten till dess startvärden
+        private void StartGame()
+        {
+            gameTimer.Start();
+
+            missedBalloons = 0;
+            score = 0;
+            intervals = 90;
+            gameIsActive = true;
+            speed = 3;
+
+        }
+        //Den här koden startar helt enkelt om spelet och tar bort allt som finns på canvasen
+        private void RestartGame()
+        {
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                itemRemover.Add(x);
+            }
+
+            foreach (Rectangle y in itemRemover)
+            {
+                MyCanvas.Children.Remove(y);
+            }
+
+            itemRemover.Clear();
+
+            StartGame();
+
+        }
+
+    }
+}
